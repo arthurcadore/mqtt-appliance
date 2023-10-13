7854b1df3b4fe4104b7c9103f3473ad024111ee2
Ubuntu <ubuntu@ip-172-31-7-171.sa-east-1.compute.internal>
2023-10-13 03:16:07 +0000

    :sparkles: added frontend to mqtt

--- src/frontend/src/db.js
+++ src/frontend/src/db.js
@@ -1,19 +1,38 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: '18.228.43.87',
-  user: 'yourUser',
-  password: 'Your#Password',
-  database: 'currentTS'
-});
+function createConnection() {
+  const connection = mysql.createConnection({
+    host: 'database',
+    user: 'yourUser',
+    password: 'Your#Password',
+    database: 'currentTS'
+  });
 
-connection.connect((error) => {
-  if (error) {
-    console.error('Erro ao conectar ao banco de dados: ' + error.message);
-  } else {
-    console.log('Conexão bem-sucedida ao banco de dados.');
-  }
-});
+  connection.connect((error) => {
+    if (error) {
+      console.error('Erro ao conectar ao banco de dados: ' + error.message);
+      // Se ocorrer um erro na conexão, tenta reconectar a cada 1 segundo
+      setTimeout(() => {
+        createConnection();
+      }, 1000);
+    } else {
+      console.log('Conexão bem-sucedida ao banco de dados.');
+    }
+  });
+
+  // Adiciona um evento de erro para evitar que o programa pare devido a erros não tratados
+  connection.on('error', (err) => {
+    console.error('Erro no banco de dados: ' + err.message);
+    // Se ocorrer um erro na conexão, tenta reconectar a cada 1 segundo
+    setTimeout(() => {
+      createConnection();
+    }, 1000);
+  });
+
+  return connection;
+}
+
+const connection = createConnection();
 
 module.exports = connection;
 
