b10b35618befc4034459af730d04f78d6e88549d
Ubuntu <ubuntu@ip-172-31-0-222.sa-east-1.compute.internal>
2023-10-13 13:34:06 +0000

    :sparkles: added graph to frontend page

--- src/frontend/src/db.js
+++ src/frontend/src/db.js
@@ -1,38 +1,19 @@
 const mysql = require('mysql2');
 
-function createConnection() {
-  const connection = mysql.createConnection({
-    host: 'database',
-    user: 'yourUser',
-    password: 'Your#Password',
-    database: 'currentTS'
-  });
+const connection = mysql.createConnection({
+  host: 'database',
+  user: 'yourUser',
+  password: 'Your#Password',
+  database: 'currentTS'
+});
 
-  connection.connect((error) => {
-    if (error) {
-      console.error('Erro ao conectar ao banco de dados: ' + error.message);
-      // Se ocorrer um erro na conexão, tenta reconectar a cada 1 segundo
-      setTimeout(() => {
-        createConnection();
-      }, 1000);
-    } else {
-      console.log('Conexão bem-sucedida ao banco de dados.');
-    }
-  });
-
-  // Adiciona um evento de erro para evitar que o programa pare devido a erros não tratados
-  connection.on('error', (err) => {
-    console.error('Erro no banco de dados: ' + err.message);
-    // Se ocorrer um erro na conexão, tenta reconectar a cada 10 segundos
-    setTimeout(() => {
-      createConnection();
-    }, 10000);
-  });
-
-  return connection;
-}
-
-const connection = createConnection();
+connection.connect((error) => {
+  if (error) {
+    console.error('Erro ao conectar ao banco de dados: ' + error.message);
+  } else {
+    console.log('Conexão bem-sucedida ao banco de dados.');
+  }
+});
 
 module.exports = connection;
 
---
+++ src/frontend/src/script.sh
@@ -0,0 +1,7 @@
+#!/bin/ash
+
+echo "Aguardando 120 segundos..."
+sleep 120
+
+echo "Executando o comando node app.js"
+node app.js
